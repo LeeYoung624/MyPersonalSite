fa1263bceda323765b50703b5260368285ec7273
PeterAlfredLee <peteralfredlee@gmail.com>
2020-12-22 20:40:31 +0800

    update birthday

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  title: 'Peterlee\'s blog',
-  description: '我的个人网站',
+  title: '宝宝&猪猪的博客',
+  description: '',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/heart.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置
   markdown: {
-    lineNumbers: false // 代码块显示行号
+    lineNumbers: true // 代码块显示行号
   },
   themeConfig: {
-    nav:[ // 导航栏配置
-      {text: '猪猪', link: '/zhuzhu/zhuzhu.md' },
-      {text: '宝宝', link: '/baobao/baobao.md'},
-      {text: '微博', link: 'https://baidu.com'},
-	  {text: 'GitHub', link: 'https://github.com/PeterAlfredLee'}
+    nav: [ // 导航栏配置
+      { text: '宝宝生日快乐', link: '/birthday/birthday.md' },
+      // { text: '宝宝', link: '/baobao/baobao.md' },
+      // { text: '微博', link: 'https://baidu.com' },
+      // { text: 'GitHub', link: 'https://github.com/PeterAlfredLee' }
     ],
     sidebar: 'auto', // 侧边栏配置
     sidebarDepth: 3, // 侧边栏显示2级
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /happy_birthday_piggy.jpeg
+heroText: 今日特别主题-宝宝生日快乐
+tagline: null
+actionText: 点这里试试 →
+actionLink: /birthday/birthday.html
+# features:
+# - title: 简洁至上
+#   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+# - title: Vue驱动
+#   details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+# - title: 高性能
+#   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: 猪猪特别呈现 | Copyright © 猪猪
+---
