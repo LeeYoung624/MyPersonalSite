903aec00641b57249496d1bf73a6585bd0b9f5d7
PeterAlfredLee <peteralfredlee@gmail.com>
2020-12-29 10:20:59 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/heart.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置
+  port:80,
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
