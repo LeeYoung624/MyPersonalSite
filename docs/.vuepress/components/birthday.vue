57e445ac0f251b7ae9c02758915da0788bf6f2ef
PeterAlfredLee <peteralfredlee@gmail.com>
2021-12-23 22:22:44 +0800

    update images

---
+++ docs/.vuepress/components/birthday.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1 style="text-align: left">祝我最爱的宝宝猪</h1>
+    <div style="margin-left: -10rem;display: flex;justify-content: space-between; width:53rem;">
+      <div style="horiz-align: left">
+        <div>
+          <img class="photo3" style="vertical-align:top;" src="../../history/pics/balidao_0.jpg"></img>
+          <img class="photo3" src="../../history/pics/balidao_6.jpg"></img>
+        </div>
+        <div>
+          <img class="photo3" style="vertical-align:top;" src="../../history/pics/qingdao_0.jpg"></img>
+          <img class="photo3" src="../../history/pics/qinglvzhao.jpg"></img>
+        </div>
+        <div>
+          <img class="photo3" style="vertical-align:bottom;" src="../../history/pics/yunfuzhao_0.jpg"></img>
+          <img class="photo3" style="vertical-align:bottom;" src="../../history/pics/xiaozhuzhu/shiyi_4.jpg"></img>
+        </div>
+      </div>
+      <div style="horiz-align: right; display: flex; flex-direction: column;">
+        <img class="photo1" src="../../history/pics/tongxinsuo.jpg"></img>
+        <img class="photo1" src="../../history/pics/xiaozhuzhu/bairi_1.jpg"></img>
+        <img class="photo1" src="../../history/pics/yunfuzhao_1.jpg"></img>
+      </div>
+    </div>
+    <h1 style="text-align: right">岁生日快乐！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "birthday.vue"
+}
+</script>
+
+<style scoped>
+.photo3 {
+  width:15rem;
+}
+
+.photo1 {
+  width:13rem;
+}
+</style>
--- docs/birthday/birthday.md
+++ docs/birthday/birthday.md
@@ -1,22 +1,10 @@
 # 宝宝生日快乐
-今天，发生了一件大事：
-
-<center><font size=10>我们家宝宝要过三十岁生日啦！</font></center>
-
-先祝我们家宝宝：
+今天，是我们家宝宝三十一岁生日
 
 <center><font size=10>生日快乐！</font></center>
 
 ![happy_birthday](./happy_birthday.png)
-
-虽然宝宝已经要奔四啦，哈哈哈哈哈
-
-![haha](./haha.gif)
-
-但是宝宝在本猪心里，宝宝永远是只**十六岁的小母猪**
-
-![xiaoxinxin](./xiaoxinxin.gif)
-
-在今天这么重要的日子里，就让我们一起回顾一下宝宝和猪猪的过去吧！
-
-[宝宝&猪猪の大事记](../history/history.md)
+</br>
+</br>
+</br>
+<birthday/>
